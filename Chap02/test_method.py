e00634433d79bde67bcdcd367c7ee6d596890394
Anthony Tastet <anthonytastet@mbp-de-myriam-2.home>
2018-06-27 19:05:28 +0200

    Chap02

--- Chap02/test_method.py
+++ Chap02/test_method.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3.4
 # -*-coding:Utf-8 
 
-class Tableau: 
-
+class Tableau:
+	"""Classe définissant une surface sur laquelle on peut écrire,
+	que l'on peut lire et effacer, par jeu de méthodes. L'attribut modifié
+	est 'surface'"""	
+	def __init__(self):
+		self.surface = ""
+	def ecrire(self, message_a_ecrire):
+		if self.surface == "" : 
+			self.surface += '\n'
+		self.surface += message_a_ecrire
+	def lire(self):
+		print(self.surface)
+	def effacer():
+		self.surface=""
+tab = Tableau()
+print(tab.surface)
+tab.ecrire("Hello world!")
+tab.lire()
+print(dir(tab))
